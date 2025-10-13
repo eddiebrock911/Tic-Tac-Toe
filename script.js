45dfd2c788f5199a1b7ee21738b20d283a4976d5
Ankit <ankitkumar823089@gmail.com>
2025-10-13 14:11:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,35 +4,6 @@ const aiModeBtn = document.getElementById('aiModeBtn');
 const friendMode = document.getElementById('friendMode');
 const aiMode = document.getElementById('aiMode');
 
-/* ===== Typing animation (hero) ===== */
-    (function typingAnim() {
-      const texts = ["Tic Tac Toe", "Creator Ankit", "Enjoy Game!"];
-      let i = 0, j = 0, isDeleting = false;
-      const el = document.querySelector('.typing');
-
-      function type() {
-        const full = texts[i];
-        if (!isDeleting) {
-          el.textContent = full.substring(0, j + 1);
-          j++;
-          if (j === full.length) {
-            isDeleting = true;
-            setTimeout(type, 1200);
-            return;
-          }
-        } else {
-          el.textContent = full.substring(0, j - 1);
-          j--;
-          if (j === 0) {
-            isDeleting = false;
-            i = (i + 1) % texts.length;
-          }
-        }
-        setTimeout(type, isDeleting ? 80 : 140);
-      }
-      type();
-    })();
-
 
 
 friendModeBtn.addEventListener('click', () => {
